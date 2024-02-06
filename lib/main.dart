b893dfbe9b2ca8b3f60470ba01c1107ef7e006b7
zayan1110 <mohamed zayan ali990@gmail.com>
2024-02-07 00:16:02 +0200

    project todo

--- lib/main.dart
+++ lib/main.dart
@@ -21,27 +21,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:hive/hive.dart';
-// import 'package:hive_flutter/adapters.dart';
-// import 'package:todo_app/pages/home_page.dart';
-
-// void main() {
-//   Hive.initFlutter();
-//   Hive.openBox('mybox');
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: HomPage(),
-//       theme: ThemeData(primarySwatch: Colors.yellow),
-//     );
-//   }
-// }
