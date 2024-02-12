61af4ed0f6f97307ccfe2f30795dd4d78216ca4b
zayan1110 <mohamedzayanali990@gmail.com>
2024-02-12 15:44:15 +0200

    project todo

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:todo_app/main.dart';
+import 'package:todo_app/to_do.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const TodoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
